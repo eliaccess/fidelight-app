be54f564709b71dfc7937e35ba24a48c4edddab1
AhmedSaeedFS <asaeed@fetchsky.com>
2021-03-31 09:47:37 +0500

    Added Package Name

--- App.tsx
+++ App.tsx
@@ -5,35 +5,22 @@
  */
 
 import React from 'react';
-
+import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { enableScreens } from 'react-native-screens';
 
-// import { initPerformance } from './app/platform/performance';
-// import { initCrashlytics } from './app/platform/crashlytics';
-// import { initAnalytics } from './app/platform/analytics';
-
-import LanguageProvider from './app/containers/LanguageProvider';
-import AppContainer from './app/containers/App';
-import { translationMessages } from './app/i18n';
 import configureStore from './app/configureStore';
 
 const initialState = {};
 const store = configureStore(initialState);
 enableScreens();
 
-// setTimeout(() => {
-//   initAnalytics();
-//   initCrashlytics();
-//   initPerformance();
-// }, 2000);
-
 function App() {
   return (
     <Provider store={store}>
-      <LanguageProvider messages={translationMessages}>
-        <AppContainer />
-      </LanguageProvider>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Welcome to FideLight</Text>
+      </View>
     </Provider>
   );
 }
